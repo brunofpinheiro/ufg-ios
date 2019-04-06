01672df7ebe4e623c228ad3cd0c9311f1aab1a41
Bruno Pinheiro <bruno.faria.pinheiro@gmail.com>
2019-04-06 15:13:54 -0300

    Add files via upload

--- CatalogoProdutos/ViewController.swift
+++ CatalogoProdutos/ViewController.swift
@@ -9,15 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var vrImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        preencheListaMaisVendidos()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func preencheListaMaisVendidos() {
+        vrImageView.image = UIImage(named: "hp1")
     }
 
 
